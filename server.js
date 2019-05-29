d96466e5fd56ab40a8d049c0e1be61b0824c68e3
naikvaishnav <naikvaishnav@gmail.com>
2019-05-29 09:18:12 +0530

    port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
 
-const port = 8000;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
